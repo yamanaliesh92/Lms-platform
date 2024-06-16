a3000bbdc3fc1a29978c9c52c3c913add3789be1
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-16 17:50:32 +0300

    seed the database wiht specific categories

---
+++ script/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engineering" },
+        { name: "Filming" },
+      ],
+    });
+    console.log("Success");
+  } catch (err) {
+    console.log("error in seed", err);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
