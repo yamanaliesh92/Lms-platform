070c3829ddabb5ad8ce3c4f534fd0874ec6cde02
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-16 17:20:17 +0300

    added uploading file componet  to component folder

---
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/file-uplaod.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { OurFileRouter, ourFileRouter } from "@/app/api/uploadthing/core";
+import { UploadDropzone } from "@/lib/uploading";
+// import { UploadDropzone } from "@/utils/uploadthing";
+import React from "react";
+import { toast } from "react-hot-toast";
+
+interface IFileUploadProps {
+  onChange: (url?: string) => void;
+  endpoint: keyof OurFileRouter;
+}
+export default function FileUpload({ onChange, endpoint }: IFileUploadProps) {
+  console.log("GGGGGGGG");
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => onChange(res?.[0].url)}
+      onUploadError={(error: Error) => {
+        toast.error(`${error?.message}`);
+      }}
+    />
+  );
+}
