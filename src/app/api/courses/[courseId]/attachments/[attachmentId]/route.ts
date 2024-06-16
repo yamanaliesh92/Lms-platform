9e9e8990f5bbbf27db30e0e83f2a492ae8c9915b
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-16 18:07:24 +0300

    added delete method to  attachment route in  api folder

---
+++ src/app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -0,0 +1,34 @@
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { courseId: string; attachmentId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json("Unauthorized", { status: 401 });
+    }
+
+    const { courseId } = params;
+    const { attachmentId } = params;
+
+    const courseOwner = await db.course.findUnique({
+      where: { id: courseId, userId },
+    });
+
+    if (!courseOwner) {
+      return NextResponse.json("Unauthorized", { status: 401 });
+    }
+    const attachment = await db.attachment.delete({
+      where: { courseId, id: attachmentId },
+    });
+    return NextResponse.json(attachment);
+  } catch (err) {
+    console.log("[COURSE_ID_ATTACHMENTS]", err);
+
+    return NextResponse.json("something went wrong", { status: 500 });
+  }
+}
