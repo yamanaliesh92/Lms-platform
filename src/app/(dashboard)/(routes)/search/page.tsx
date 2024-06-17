b4243b88838db29d6f1e67e587e726555e35baf6
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-17 23:10:15 +0300

    added get-course file in action folder

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,40 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import Categories from "./_components/categories";
+import { redirect } from "next/navigation";
+import SearchInput from "../../_components/search-input";
 
-export default function SearchPage() {
-  return <div className="p-6">search</div>;
+interface SearchPageProps {
+  searchParams: { title: string; categoryId: string };
+}
+
+export default async function page({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({ orderBy: { name: "asc" } });
+
+  const { title, categoryId } = searchParams;
+
+  const courses = await getCourses({
+    categoryId: categoryId,
+    userId: userId,
+    title: title,
+  });
+
+  console.log("GET", courses);
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories items={categories} />
+      </div>
+      <CoursesList items={courses} />
+    </>
+  );
 }
