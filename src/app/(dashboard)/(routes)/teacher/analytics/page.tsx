1058837076fcca8b7d4a55dbb17d5e904ad31b87
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-18 00:20:42 +0300

    finished pageAnalytics page

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import Chart from "./_components/chart";
+import DataCard from "./_components/data-card";
+import { redirect } from "next/navigation";
 
-export default function AnalyticsPage() {
-  return <div>here</div>;
+export default async function page() {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const userId = "1";
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  const newLocal = "Total Sales";
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label={newLocal} value={totalSales} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 }
