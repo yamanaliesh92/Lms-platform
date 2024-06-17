bf82ef4b7fef91b52029acb3e86dfe964e0fb15d
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-17 13:00:47 +0300

    added delete-method for course

---
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/action.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import ConfirmModal from "@/components/modals/confrim-modals";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useConfettiStore } from "../../../../../../../../hook/use-confetti-store";
+
+interface IActionProps {
+  disable: boolean;
+  courseId: string;
+  isPublished: boolean;
+}
+
+export default function ChapterActions({
+  disable,
+  courseId,
+  isPublished,
+}: IActionProps) {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`http://localhost:3000/api/courses/${courseId}`);
+      toast.success("Course deleted");
+      router.refresh();
+      router.push("/dash/teacher/course");
+    } catch {
+      toast.error("something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      if (isPublished) {
+        await axios.patch(
+          `http://localhost:3000/api/courses/${courseId}/unpublish`
+        );
+        toast.success("Course unPublished");
+      } else {
+        await axios.patch(
+          `http://localhost:3000/api/courses/${courseId}/publish`
+        );
+        toast.success("Course published");
+        confetti.onOpen();
+      }
+
+      router.refresh();
+    } catch {
+      toast.error("some thing went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disable || isLoading}
+        variant="outline"
+        size={"sm"}
+      >
+        {isPublished ? "Unpublished" : "published"}
+      </Button>
+
+      <ConfirmModal onConfirm={onDelete}>
+        <Button disabled={isLoading} size={"sm"}>
+          <Trash className="h-4 w-4 " />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+}
--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -3,6 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
+import Mux from "@mux/mux-node";
+
+const { video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET! as string
+);
+
 export async function PATCH(
   req: NextRequest,
   { params }: { params: { courseId: string } }
@@ -14,11 +21,53 @@ export async function PATCH(
     }
     const value = await req.json();
 
-    const course = await db.course.update({
-      where: { id: params.courseId, userId },
+    const save = await db.course.update({
+      where: { id: params.courseId, userId: "1" },
       data: { ...value },
     });
-    return NextResponse.json(course);
+    return NextResponse.json(save);
+  } catch (err) {
+    console.log("[course]", err);
+
+    return NextResponse.json("something went wrong", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json("Unauthorized", { status: 401 });
+    }
+
+    const { courseId } = params;
+
+    const course = await db.course.findUnique({
+      where: { id: params.courseId, userId },
+      include: {
+        chapters: {
+          include: {
+            maxData: true,
+          },
+        },
+      },
+    });
+    if (!course) {
+      return NextResponse.json("not found", { status: 404 });
+    }
+
+    for (const chapter of course.chapters) {
+      if (chapter.maxData?.assetId) {
+        await video.assets.delete(chapter.maxData.assetId);
+      }
+    }
+    const deleteCourse = await db.course.delete({
+      where: { id: courseId },
+    });
+    return NextResponse.json(deleteCourse);
   } catch (err) {
     console.log("[course]", err);
 
