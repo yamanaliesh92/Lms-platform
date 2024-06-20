ee559c345bf59085712557884db8aa9ab008b0fd
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-20 13:43:35 +0300

    added ConfettiProvider to layout root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ToasterProvider from "./(dashboard)/_components/providers/toast-provider";
+import ConfettiProvider from "./(dashboard)/_components/providers/confetti-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ToasterProvider />
+          <ConfettiProvider />
           {children}
         </body>
       </html>
